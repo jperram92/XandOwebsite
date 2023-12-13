5fe31d213465f04f95027694e0627dc5556bdf3a
jperram92 <93854423+jperram92@users.noreply.github.com>
2023-12-14 00:17:25 +1100

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,48 @@
+let currentPlayer = 'X';
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
+let gameActive = true;
+
+function placeMarker(cell) {
+    const index = Array.from(cell.parentNode.children).indexOf(cell);
+
+    if (gameBoard[index] === '' && gameActive) {
+        gameBoard[index] = currentPlayer;
+        cell.textContent = currentPlayer;
+        
+        if (checkWin()) {
+            document.getElementById('result').textContent = `${currentPlayer} wins!`;
+            gameActive = false;
+        } else if (gameBoard.every(cell => cell !== '')) {
+            document.getElementById('result').textContent = 'It\'s a draw!';
+            gameActive = false;
+        } else {
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        }
+    }
+}
+
+function checkWin() {
+    const winPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+
+    return winPatterns.some(pattern => {
+        const [a, b, c] = pattern;
+        return gameBoard[a] !== '' && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c];
+    });
+}
+
+function resetGame() {
+    currentPlayer = 'X';
+    gameBoard = ['', '', '', '', '', '', '', '', ''];
+    gameActive = true;
+
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.textContent = '';
+    });
+
+    document.getElementById('result').textContent = '';
+}
